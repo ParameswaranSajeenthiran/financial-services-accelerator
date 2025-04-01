350c88b4c77dacd33a96584d62bc60df9cb2a062
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-04-01 14:47:10 +0530

    updated workflow script

--- test.sh
+++ test.sh
@@ -59,9 +59,9 @@ sudo mkdir -p /home/$name/.wso2-updates/docker && sudo chmod -R 777 /home/$name/
 $TEST_HOME/wso2is-7.0.0/bin/wso2update_linux --username $USERNAME --password $PASSWORD ||  ($TEST_HOME/wso2is-7.0.0/bin/wso2update_linux --username $USERNAME --password $PASSWORD )
 #
 echo '##################### Moving Packs to RUNNER_HOME #####################'
-unzip financial-services-accelerator/accelerators/fs-is/target/wso2-fsiam-accelerator-4.0.0-M3.zip -d $TEST_HOME/wso2is-7.0.0/
-#wget https://github.com/ParameswaranSajeenthiran/files/raw/master/wso2-fsiam-accelerator-4.0.0-M3.zip -O wso2-fsiam-accelerator-4.0.0-M3.zip
-#unzip wso2-fsiam-accelerator-4.0.0-M3.zip -d $TEST_HOME/wso2is-7.0.0/
+#unzip financial-services-accelerator/accelerators/fs-is/target/wso2-fsiam-accelerator-4.0.0-M3.zip -d $TEST_HOME/wso2is-7.0.0/
+wget https://github.com/ParameswaranSajeenthiran/files/raw/master/wso2-fsiam-accelerator-4.0.0-M3.zip -O wso2-fsiam-accelerator-4.0.0-M3.zip
+unzip wso2-fsiam-accelerator-4.0.0-M3.zip -d $TEST_HOME/wso2is-7.0.0/
 
 echo '##################### Setup MYSQL #####################'
 sudo apt-get update
@@ -176,7 +176,10 @@ echo '##################### Update deployment.toml #####################'
 rm -f $TEST_HOME/wso2is-7.0.0/repository/conf/deployment.toml
 # copy the new deployment.toml
 cp $RUNNER_HOME/deployment.toml $TEST_HOME/wso2is-7.0.0/repository/conf/deployment.toml
-#
+
+cat $RUNNER_HOME/deployment.toml
+cat $TEST_HOME/wso2is-7.0.0/repository/conf/deployment.toml
+
 #sed -i '/\[oauth\.oidc\]/,/^\s*$/d' $TEST_HOME/wso2is-7.0.0/repository/conf/deployment.toml
 #sed -i '/\[financial_services\.service\.extensions\.endpoint]/,/^\s*$/d' $TEST_HOME/wso2is-7.0.0/repository/conf/deployment.toml
 #sed -i '/\[financial_services\.service\.extensions\.endpoint\.security]/,/^\s*$/d' $TEST_HOME/wso2is-7.0.0/repository/conf/deployment.toml
